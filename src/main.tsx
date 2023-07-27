3d2f5f8724ccf392c1af3010a4b50127ef2ad93d
MFCARNEIRO <mfcarneiro@brasilseg.com>
2023-07-27 17:22:52 -0300

    init single spa

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import App from "./App.tsx";
+import "./index.css";
+import singleSpaReact from "single-spa-react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import ReactDOM from "react-dom";
+
+const reactlifecycles = singleSpaReact({
+  React,
+  ReactDOM,
+  rootComponent: App,
+  domElementGetter: () => document.getElementById("ul_main"),
+});
+
+export const bootstrap = [reactlifecycles.bootstrap];
+
+export const mount = [reactlifecycles.mount];
+
+export const unmount = [reactlifecycles.unmount];
